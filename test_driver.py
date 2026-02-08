549ca4667c6fd609739422be1234efcbdea87edb
charmi-reddy <23ra1a6620@kpritech.ac.in>
2026-02-08 14:45:01 +0530

    Fixed Chrome path issue - let Selenium auto-detect Chrome

---
+++ test_driver.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.binary_location = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
+
+driver = webdriver.Chrome(options=options)
+driver.get("https://www.google.com")
